def07af221357f9e27d2c226dd3974ec96f8cafe
ak4422920 <ak4422920@gmail.com>
2025-03-27 09:34:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ ADMIN = int(getenv("ADMIN", "7251898668"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
 API_ID = int(getenv("API_ID", "29171167"))
 API_HASH = str(getenv("API_HASH", "7ea2149629e445936619f06a3c0dc716"))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7609220129:AAH9yr0-WDBjCycA2WT2HNHLnRvYLMvVpQo"))
 FORCE_SUB = os.environ.get("FORCE_SUB", "-1002442422204") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://ai:ai@cluster0.fgu4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
